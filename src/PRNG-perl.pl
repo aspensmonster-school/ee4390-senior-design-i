c389ee6328893d2b096c5b576980ad77d20e6c3e
Preston Maness <aggroskater@gmail.com>
2013-09-15 21:56:08 -0500

    Updating PRNG-perl.pl to output bitstreams large enough for diehard to work with. Test results don't look promising so far. More PASSED tests, but still plenty of FAILED tests.

--- src/PRNG-perl.pl
+++ src/PRNG-perl.pl
@@ -12,7 +12,11 @@ my $rand;
 
 open(my $outputFile, '>:raw', 'perl-rand.bin') or die "Can't open: $!";
 
-for (my $i = 0 ; $i < 1000000 ; $i++)
+
+# 1 billion integers will output a file that's about 4 gigs in size. This 
+# will take at least a handful of minutes to finish on a modern (multi-core, 
+# 2+ GHz, 3+ GB RAM, SATA drives) machine.
+for (my $i = 0 ; $i < 1000000000 ; $i++)
 {
   $rand = int(rand(0x100000000));
   print $outputFile pack('L', $rand);
