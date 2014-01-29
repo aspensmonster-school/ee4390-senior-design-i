c6bc4a31d3f572ea887d966f5177cb3295f7e404
Preston Maness <aggroskater@gmail.com>
2014-01-29 04:10:36 -0600

    Oops. That's better.

--- src/arduino_code/arduino_code.ino
+++ src/arduino_code/arduino_code.ino
@@ -4,18 +4,18 @@ void setup() {
 }
 
 void loop() {
-  byte randomByte;
-//  byte randomByte = digitalRead(2) | (digitalRead(2)<<1) | 
-//  (digitalRead(2)<<2) | (digitalRead(2)<<3) | (digitalRead(2)<<4) |
-//  (digitalRead(2)<<5) | (digitalRead(2)<<6) | (digitalRead(2)<<7);
+//  byte randomByte;
+  byte randomByte = digitalRead(2) | (digitalRead(2)<<1) | 
+  (digitalRead(2)<<2) | (digitalRead(2)<<3) | (digitalRead(2)<<4) |
+  (digitalRead(2)<<5) | (digitalRead(2)<<6) | (digitalRead(2)<<7);
 
-  randomByte = 56;
+//  randomByte = 56;
 
   Serial.write(randomByte);
 
-  randomByte = 55;
+//  randomByte = 55;
   
-  Serial.write(randomByte);
+//  Serial.write(randomByte);
 
   delay(10);
  
