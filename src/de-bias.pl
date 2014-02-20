a7eb18be7d80d6c9cba08597ddec0f6a2e23c3fe
Preston Maness <aggroskater@gmail.com>
2014-02-19 23:27:36 -0600

    It's looking like my bitstream might be fine, but that it just needs to be debiased. I'm going to give that a go and see what the test results look like.

---
+++ src/de-bias.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+#This script should eventually debias the bitstream.
+
+use warnings;
+use strict;
+
+my $in = "output.after-comparator-fix";
+my $out = "output-debias";
+
+open(my $outputFile, '>', $out) or die "Can't open: $!";
+open(my $inputFile, '<', $in) or die "Can't open: $!";
+
+binmode($inputFile);
+binmode($outputFile);
+
+my $file; { local $/ ; $file = <$inputFile>;}
+my $binary = unpack('B*',$file);
+
+#if you don't do this then the contents get held in a buffer until the file 
+#closes. As you can see, the file never closes! Shame on me...
+
+print $outputFile pack('B*',$binary);
