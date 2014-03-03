26960684edf33c4898714defcf74ff84c795bf6a
Preston Maness <aggroskater@gmail.com>
2014-03-02 23:26:47 -0600

    Going to see if this debiasing/decorrelating code is helpful or not.

--- src/arduino_code/arduino_code.ino
+++ src/arduino_code/arduino_code.ino
@@ -4,14 +4,45 @@ void setup() {
 }
 
 void loop() {
-//  byte randomByte;
-  byte randomByte = digitalRead(2) | (digitalRead(2)<<1) | 
-  (digitalRead(2)<<2) | (digitalRead(2)<<3) | (digitalRead(2)<<4) |
-  (digitalRead(2)<<5) | (digitalRead(2)<<6) | (digitalRead(2)<<7);
+
+  byte randomByte;
+  int shift = 0;
+
+  //while randomByte isn't filled with debiased bits...
+  while ( shift < 8) {
+
+    //Start Von Neumann debiasing routine. Sample two bits.
+    byte randOne = digitalRead(2) & 0x01;
+    byte randTwo = digitalRead(2) & 0x01;
+
+    //Bits were same value. discard.
+    if (randOne == randTwo) {
+      continue;
+    }
+    //went from one to zero. Write one.
+    else if (randOne > randTwo) {
+      randomByte = randomByte | (randOne<<shift);
+      shift++;
+    }
+    //went from zero to one. Write zero.
+    else if (randTwo > randOne) {
+      randomByte = randomByte | (randOne<<shift);
+      shift++;
+    }
+    //If randomByte is filled, write it out to serial.
+    if ( shift == 7 ) {
+      Serial.write(randomByte);
+    }
+    //else, keep going.
+    else {
+      continue;
+    }
+
+  } //end while; a full, debiased byte has been written.
 
 //  randomByte = 56;
 
-  Serial.write(randomByte);
+//  Serial.write(randomByte);
 
 //  randomByte = 55;
   
